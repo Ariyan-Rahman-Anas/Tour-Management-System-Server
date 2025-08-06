c9c5dd5edef5928d3d900f2fb2f310cf73d1c9d9
Your Personal Name <your.personal@email.com>
2025-08-06 23:56:33 +0600

    create new API for tour types

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { UserRoute } from "../modules/user/user.route";
 import { AuthRoute } from "../modules/auth/auth.route";
 import { DivisionRoute } from "../modules/division/division.route";
+import { TourRoute } from "../modules/tour/tour.route";
 
 export const router = Router()
 
@@ -18,6 +19,10 @@ const moduleRoutes = [
         path: "/division",
         route: DivisionRoute
     },
+    {
+        path: "/tour",
+        route: TourRoute
+    }
 ]
 
 moduleRoutes.forEach((route) => {
